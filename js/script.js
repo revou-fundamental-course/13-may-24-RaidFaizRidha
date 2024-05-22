99892a12a4127b47a9da35aff641be025bc0c056
RaidFaizRidha <raidfaizridha09@gmail.com>
2024-05-22 19:00:46 +0700

    commit done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
 // ini js
 
-//  function hasil(){
-//     let alas = document.getElementById("alasSegitiga").Value;
-//     let tinggi = document.getElementById("tinggiSegitiga").Value;
-//     let hasil = parseInt(alasSegitiga) * parseInt(tinggiSegitiga)
-
-//     console.log(hasil)
-//  }
-
 function luasSegitiga() {
    let alas = document.getElementById("alas").value;
    let tinggi = document.getElementById("tinggi").value;
    let luas = 0.5 * alas * tinggi;
    document.getElementById(
      "hasilLuas"
-   ).innerHTML = `HASIL LUAS : <br> ${0.5} x ${alas} x ${tinggi} = ${luas}`;
+   ).innerHTML = `Hasil luas segitiga : <br> Alas: ${alas} <br> Tinggi: ${tinggi} <br><br> Hasil: <br> ${0.5} x ${alas} x ${tinggi} = ${luas}`;
    }
 
    function resetLuas() {
       document.getElementById("alas").value = "";
       document.getElementById("tinggi").value = "";
-      document.getElementById("hasilLuas").innerHTML = "HASIL LUAS : ";
+      document.getElementById("hasilLuas").innerHTML = "Hasil luas segitiga : ";
     }
+
+    function kelilingSegitiga() {
+      let sisi1 = document.getElementById("sisi1").value;
+      let sisi2 = document.getElementById("sisi2").value;
+      let sisi3 = document.getElementById("sisi3").value;
+      let keliling = parseInt(sisi1) + parseInt(sisi2) + parseInt(sisi3)
+      document.getElementById(
+        "hasilKeliling"
+      ).innerHTML = `Hasil keliling segitiga : <br> Sisi 1: ${sisi1} <br> Sisi 2: ${sisi2} <br> Sisi 3: ${sisi3} <br><br> Hasil: <br> ${sisi1} + ${sisi2} + ${sisi3} = ${keliling}`;
+      }
+
+      function resetKeliling() {
+        document.getElementById("sisi1").value = "";
+        document.getElementById("sisi2").value = "";
+        document.getElementById("sisi3").value = "";
+        document.getElementById("hasilKeliling").innerHTML = "Hasil keliling segitiga : ";
+      }
